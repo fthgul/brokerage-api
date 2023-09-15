27facc92b8c94701fe338ef554d4a6db7d956a15
fatihgul <fatih.gul@getir.com>
2023-09-15 14:51:07 +0300

    1- [Setup] Initial project structure and added required dependencies
    - Set up the base package structure including configuration, model, controller, service, repository, and util packages.
    - Added necessary dependencies to the `pom.xml` for Spring Boot, database connectors, and other utilities.

---
+++ src/test/java/com/midas/studycase/brokerageapi/BrokerageApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.midas.studycase.brokerageapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrokerageApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
